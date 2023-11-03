5e8164d2dfa7877fc80fcdc314a4316b60b9dbe5
SvitLana1990 <svitlana112@outlook.es>
2023-11-03 23:41:01 +0100

    will-start

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    white: '#ffffff',
+    grey: '#C0C0C0',
+    orange: '#FFA500',
+    red: '#FF0000',
+    green: '#32CD32',
+    blue: '#000080',
+    black: '#000000',
+  },
+  spacing: value => `${value * 4}px`,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
