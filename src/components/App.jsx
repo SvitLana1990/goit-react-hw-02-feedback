40d47893c6326d29b10459511b6097bdf082f6ac
SvitLana1990 <svitlana112@outlook.es>
2023-11-04 15:22:27 +0100

    done-first-task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,11 @@ import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notofocation';
 import { GlobalStyle } from 'GlobalStyle';
 import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 
-const Button = ({ label, onUpdate }) => {
-  return <button onClick={onUpdate}>{label}</button>;
-};
+// const Button = ({ label, onUpdate }) => {
+//   return <button onClick={() => onUpdate(label.toLowerCase())}>{label}</button>;
+// };
 
 export class App extends Component {
   state = {
@@ -15,29 +16,35 @@ export class App extends Component {
     bad: 0,
   };
 
-  updateGood = () => {
-    this.setState(prevState => {
-      return {
-        good: prevState.good + 1,
-      };
-    });
+  onLeaveFeedback = type => {
+    this.setState(prevState => ({
+      [type]: prevState[type] + 1,
+    }));
   };
 
-  updateNeutral = () => {
-    this.setState(prevState => {
-      return {
-        neutral: prevState.neutral + 1,
-      };
-    });
-  };
+  // updateGood = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       neutral: prevState.neutral + 1,
+  //     };
+  //   });
+  // };
 
-  updateBad = () => {
-    this.setState(prevState => {
-      return {
-        bad: prevState.bad + 1,
-      };
-    });
-  };
+  // updateNeutral = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       neutral: prevState.neutral + 1,
+  //     };
+  //   });
+  // };
+
+  // updateBad = () => {
+  //   this.setState(prevState => {
+  //     return {
+  //       bad: prevState.bad + 1,
+  //     };
+  //   });
+  // };
 
   countTotalFeedback = () => {
     const { good, neutral, bad } = this.state;
@@ -54,16 +61,18 @@ export class App extends Component {
     const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
     const positivePercentage = this.countPositiveFeedbackPercentage();
+    const options = ['good', 'neutral', 'bad'];
+
     return (
       <div>
         <Section title="Please leave your feedback">
-          <Button label="Good" value={good} onUpdate={this.updateGood} />
-          <Button
-            label="Neutral"
-            value={neutral}
-            onUpdate={this.updateNeutral}
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
           />
-          <Button label="Bad" value={bad} onUpdate={this.updateBad} />
+          {/* <Button label="Good" value={good} onUpdate={this.updateGood} />
+          <Button label="Neutral" value={neutral} onUpdate={this.updateNeutral} />
+          <Button label="Bad" value={bad} onUpdate={this.updateBad} /> */}
         </Section>
         <Section title="Statistics">
           {total > 0 && (
